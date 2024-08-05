cfe69ce3ce6e9718f3b69fd71a109be727b6132e
bdimon <dmytr0b@yahoo.com>
2024-08-05 19:53:55 +0200

    add routes form product

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import './App.css';
 import {useEffect} from "react";
 import { useTelegram } from './hooks/useTelegram';
 import Header from './components/Header/Header';
-import Button from './components/Button/Button';
+// import Button from './components/Button/Button';
+import {Route, Routes} from 'react-router-dom';
+import ProductList from './components/ProductList/ProductList';
+import Form from './components/Form/Form';
+
 // const tlg = window.Telegram.WebApp;
-// window.Telegram.WebApp.
 
 function App() {
   const {tlg, onToggleButton} = useTelegram();
@@ -16,7 +19,12 @@ function App() {
   return (
     <div className="App">
     <Header />
-      <Button onClick={onToggleButton}>Toggle</Button>
+      {/* <Button onClick={onToggleButton}>Toggle</Button> */}
+      <Routes>
+        <Route index element={<ProductList />}/>
+        <Route path={'form'} element={<Form />}/>
+
+      </Routes>
     </div>
   );
 }

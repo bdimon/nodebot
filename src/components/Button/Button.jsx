b92aacc2b1bfe3359e49b6c151a0e275da7b6c3c
bdimon <dmytr0b@yahoo.com>
2024-08-05 21:59:59 +0200

    change button class

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Button = (props) => {
     return (
-        <button {...props} className={'button ' + props.className}/>
+        // <button {...props} className={'button ' + props.className}/>
+        <button {...props} className={'button'}/>
     );
 };
 export default Button;
